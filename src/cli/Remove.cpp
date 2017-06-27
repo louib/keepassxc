9f32b839ca7136bcd483ce05a57992b6513b0fed
Louis-Bertrand Varin <louisbvarin@gmail.com>
2017-06-27 17:12:19 -0400

    Missing files.

---
+++ src/cli/Remove.cpp
@@ -0,0 +1,122 @@
+/*
+ *  Copyright (C) 2017 KeePassXC Team <team@keepassxc.org>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 2 or (at your option)
+ *  version 3 of the License.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <cstdlib>
+#include <stdio.h>
+
+#include "Remove.h"
+
+#include <QCommandLineParser>
+#include <QCoreApplication>
+#include <QStringList>
+#include <QTextStream>
+
+#include "core/Database.h"
+#include "core/Entry.h"
+#include "core/Group.h"
+#include "core/Tools.h"
+#include "keys/CompositeKey.h"
+#include "cli/Utils.h"
+
+Remove::Remove()
+{
+    this->name = QString("rm");
+    this->shellUsage = QString("rm entry_path");
+    this->description = QString("Remove an entry from the database.");
+}
+
+Remove::~Remove()
+{
+}
+
+int Remove::execute(int argc, char** argv)
+{
+    QCoreApplication app(argc, argv);
+    QTextStream out(stdout);
+
+    QCommandLineParser parser;
+    parser.setApplicationDescription(QCoreApplication::translate("main", "Remove an entry from the database."));
+    parser.addPositionalArgument("database", QCoreApplication::translate("main", "Path of the database."));
+    parser.addPositionalArgument("entry", QCoreApplication::translate("main", "Path of the entry to remove."));
+    parser.process(app);
+
+    const QStringList args = parser.positionalArguments();
+    if (args.size() != 2) {
+        parser.showHelp(EXIT_FAILURE);
+    }
+
+    out << "Insert the database password\n> ";
+    out.flush();
+
+    QString line = Utils::getPassword();
+    CompositeKey key = CompositeKey::readFromLine(line);
+
+    Database* db = Database::openDatabaseFile(args.at(0), key);
+    if (db == nullptr) {
+        return EXIT_FAILURE;
+    }
+
+    return this->removeEntry(db, args.at(0), args.at(1));
+
+}
+
+int Remove::executeFromShell(Database* database, QString databasePath, QStringList arguments)
+{
+
+    QTextStream outputTextStream(stdout, QIODevice::WriteOnly);
+    if (arguments.size() != 1) {
+        outputTextStream << this->shellUsage << "\n";
+        outputTextStream.flush();
+        return EXIT_FAILURE;
+    }
+    return this->removeEntry(database, databasePath, arguments.at(0));
+}
+
+int Remove::removeEntry(Database* database, QString databasePath, QString entryPath)
+{
+
+    QTextStream outputTextStream(stdout, QIODevice::WriteOnly);
+    Entry* entry = database->rootGroup()->findEntry(entryPath);
+    if (!entry) {
+        qCritical("Entry %s not found.", qPrintable(entryPath));
+        return EXIT_FAILURE;
+    }
+
+    Utils::createRecycleBin(database);
+
+    QString entryTitle = entry->title();
+    if (Tools::hasChild(database->metadata()->recycleBin(), entry) ||
+        !database->metadata()->recycleBinEnabled()) {
+        if (!Utils::askYesNoQuestion("You are about to remove entry " + entryTitle + " permanently.", true)) {
+            return EXIT_FAILURE;
+        }
+        delete entry;
+    } else {
+        database->recycleEntry(entry);
+    };
+
+    QString errorMessage = database->saveToFile(databasePath);
+    if (!errorMessage.isEmpty()) {
+        qCritical("Unable to save database to file : %s", qPrintable(errorMessage));
+        return EXIT_FAILURE;
+    }
+    outputTextStream << "Successfully removed entry " << entryTitle << ".\n";
+    outputTextStream.flush();
+
+    return EXIT_SUCCESS;
+
+}
