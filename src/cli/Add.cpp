ddab032d5935638485a44624fded81701f2c91f1
Louis-Bertrand Varin <louisv@unito.io>
2017-06-27 19:14:31 -0400

    PasswordGenerator::DefaultLength

--- src/cli/Add.cpp
+++ src/cli/Add.cpp
@@ -110,7 +110,7 @@ int Add::addEntry(Database* database, QString databasePath, QString entryPath)
     QString password;
     if (Utils::askYesNoQuestion("Do you want to generate a new password?")) {
         PasswordGenerator passwordGenerator;
-        passwordGenerator.setLength(20);
+        passwordGenerator.setLength(PasswordGenerator::DefaultLength);
         passwordGenerator.setCharClasses(PasswordGenerator::LowerLetters | PasswordGenerator::UpperLetters |
                                          PasswordGenerator::Numbers | PasswordGenerator::SpecialCharacters);
 
